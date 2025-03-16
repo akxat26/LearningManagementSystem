d0c3a5fd0fa6d4a73af072a02c185c4cb50bfe99
Akshat Chandra <142999870+akxat26@users.noreply.github.com>
2025-03-16 13:06:36 +0530

    Delete frontend/build directory

--- frontend/build/src/Components/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ErrorPage() {
-  return (
-    <div style={{paddingTop:'50px'}}><h2 style={{color:'red'}}>Error Page...</h2></div>
-  )
-}
-
-export default ErrorPage;
